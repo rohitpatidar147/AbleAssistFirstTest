d6e5d938a269ab458acc64f65348c887e7bfa653
rohitpatidar147 <rpatida@rockets.utoledo.edu>
2026-01-14 12:00:00 -0400

    Create centralized application asset registry

---
+++ config/assets.js
@@ -0,0 +1,23 @@
+export const APP_ASSETS = {
+  onboarding: {
+    welcome: require('../assets/onboarding_welcome.jpg'),
+    asl: require('../assets/onboarding_asl.jpg'),
+    tts: require('../assets/onboarding_tts.jpg'),
+    stt: require('../assets/onboarding_stt.jpg'),
+  },
+  home: {
+    tts: require('../assets/card_tts.jpg'),
+    stt: require('../assets/card_stt.jpg'),
+    asl: require('../assets/card_asl.jpg'),
+    beMyEyes: require('../assets/card_bemyeyes.jpg'),
+    aiAssistant: require('../assets/card_ai.jpg'),
+  },
+  chat: {
+    aiAvatar: require('../assets/ai_avatar.jpg'),
+    userAvatar: require('../assets/user_avatar.jpg'),
+  },
+  media: {
+    mic: require('../assets/icon_mic.jpg'),
+    camera: require('../assets/icon_camera.jpg'),
+  },
+};
